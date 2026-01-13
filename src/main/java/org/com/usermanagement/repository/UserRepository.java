6d619aa0f592d1bad7ebf9235069af383355a4b0
Kunal Titare <kunaltitare213@gmail.com>
2026-01-13 16:10:31 +0530

    adding jwt token authentication

--- src/main/java/org/com/usermanagement/repository/UserRepository.java
+++ src/main/java/org/com/usermanagement/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package org.com.usermanagement.repository;
 
-import org.com.usermanagement.userEntity.User;
+import org.com.usermanagement.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
